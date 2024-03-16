fd5967c22a66031d79244a7db238833af0a0ed55
Marc Paterno <paterno@fnal.gov>
2024-03-16 08:06:16 -0500

    Test %nin% with empty vector

--- tests/testthat/test-listing.R
+++ tests/testthat/test-listing.R
@@ -4,6 +4,12 @@ test_that("nin works", {
   res <- x %nin% table
   expect_vector(res, ptype=logical())
   expect_identical(res, c(FALSE, TRUE))
+
+  empty_table <- integer()
+  empty_res <- x %nin% empty_table
+  expect_length(empty_res, 2)
+  expect_vector(empty_res, ptype=logical())
+  expect_identical(empty_res, c(TRUE, TRUE))
 })
 
 
